471ad5529a99b95de6a5776e40f70ab43f6ce03f
Administrator <253123123@qq.com>
2016-03-23 14:56:15 +0800

    RxJava 2. 异步回调(好处:无阻塞式)

--- app/src/main/java/com/user/hilo/RxJava/Api.java
+++ app/src/main/java/com/user/hilo/RxJava/Api.java
@@ -11,9 +11,26 @@ import java.util.List;
  * 我们有个 Web API，能根据给定的查询请求搜索到整个互联网上猫的图片。
  * 每个图片包含可爱指数的参数（描述图片可爱度的整型值）。
  * 我们的任务将会下载到一个猫列表的集合，选择最可爱的那个，然后把它保存到本地。
+ *
+ * 2.走向异步
+     要知道我们身在一个对等待很敏感的世界里，我们也知道不可能只有阻塞式的调用。在 Android 中我们也总需要处理异步代码。
+     拿 Android 的 OnClickListener 举个例子，当你需要处理一个控件的点击事件时，你必须提供一个监听器（回调）以供在用户点击控件时被调用。这没有理由使用阻塞的方式去接受点击事件的回调，所以对点击来说总是异步的。现在，让我们也使用异步编程吧。
+     异步的网络调用
+     开始想象下使用没有阻塞的 HTTP client（例如Ion），还有就是我们的cats-sdk.jar已经更新。它的 API 也换成了异步的方式调用。
+     新 API 的接口：
  */
 public interface Api {
-    // 传统风格的api,他被打包进cat-sdk.jar中
+
+    /* 传统api
     List<Cat> queryCats(String query);
+    Uri store(Cat cat);*/
+
+    interface CatsQueryCallback {
+        void onCatsListReceived(List<Cat> cats);
+        void onError(Exception e);
+    }
+
+    void queryCats(String query, CatsQueryCallback catsQueryCallback);
+
     Uri store(Cat cat);
 }
--- app/src/main/java/com/user/hilo/RxJava/CatsHelper.java
+++ app/src/main/java/com/user/hilo/RxJava/CatsHelper.java
@@ -9,24 +9,15 @@ import java.util.List;
 
 /**
  * Created by Administrator on 2016/3/23.
- * 唉，这样清晰简单的代码帅到让我窒息啊。来理清一下代码的炫酷之处吧。
- * 主方法 saveTheCutestCat 只包含了 3 个其它方法，然后花个几分钟来看看代码和思考这些方法。
- * 你给方法提供了输入参数然后就能得到结果返回了，在这个方法工作的时候我们需要等待它的完成。
-
-     简洁而有用，让我们再看看组合方法的其它优势：
-     组合
-     正如我们看到的，根据其它 3 个方法而新创建了一个方法（saveTheCutestCat），因此我们组合了它们。
-     像乐高积木那样，我们把方法之间连接起来组成了乐高积木（实际上可以在之后组合起来）。组合方法是很简单的，从一个方法得到返回结果然后再把它传递给另外的方法做为输入参数，这不简单吗？
-     错误的传递
-     另外一个好处就是我们处理错误的方式了。任何一个方法都可能因执行时发生错误而被终止，
-     这个错误能在任何层次上被处理掉，Java 中我们叫它抛出了异常，然后这个错误在 try/catch 代码块中做处理。
-     这里的关键点是我们不需要为组合方法里的每个方法都做异常处理，仅需要对这些组合起来的方法做统一处理，像下面这样：
+ *  所以现在我们能异步的获取猫的信息集合列表了，返回正确或错误的结果时都会通过 CatsQueryCallback回调接口。
+    因为 API 改变了所以我们也不得不改变我们的CatsHelper:
  */
 public class CatsHelper {
 
     Api api;
 
-    public Uri saveTheCutestCat(String query) {
+   /* 传统:
+   public Uri saveTheCutestCat(String query) {
         try {
             List<Cat> cats = api.queryCats(query);
             Cat cat = findCutest(cats);
@@ -36,6 +27,45 @@ public class CatsHelper {
             e.printStackTrace();
             return Uri.parse("");
         }
+    }
+
+    private Cat findCutest(List<Cat> cats) {
+        return Collections.max(cats);
+    }*/
+
+
+    interface CutestCatCallback {
+        void onCutestCatSaved(Uri uri);
+        void onQueryFailed(Exception e);
+    }
+
+
+
+    /**
+     * ps: 因为异步的原因, 取消了返回值Uri, 提供了返回Uri的接口.
+     * 现在我们已经不能使用阻塞的 API 了，
+     * 我们也不能把我们的客户端上写成阻塞式的调用（实际上是可以的，但需要明确的在线程中使用synchronized、CountdownLatch、等等，也需要在下层中使用异步处理）。
+     * 所以我们不能在 saveTheCutestCat方法中直接返回一个值，我们需要对它进行异步回调处理。让我们再深入一点，
+     * 如果我们 API 的两个方法调用都是异步的，举个例子，我们正在使用非阻塞IO去写进一个文件等。
+     * @param query
+     * @param cutestCatCallback
+     */
+    public void saveTheCutestCat(String query, final CutestCatCallback cutestCatCallback) {
+        api.queryCats(query, new Api.CatsQueryCallback() {
+            @Override
+            public void onCatsListReceived(List<Cat> cats) {
+                Cat cat = findCutest(cats);
+                Uri saveUri = api.store(cat);
+                cutestCatCallback.onCutestCatSaved(saveUri);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                cutestCatCallback.onQueryFailed(e);
+            }
+        });
+
+
     }
 
     private Cat findCutest(List<Cat> cats) {
