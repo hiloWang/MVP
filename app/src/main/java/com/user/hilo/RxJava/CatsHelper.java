2b2dbb663b0f5d543419cbaa94e63e608660e440
Administrator <253123123@qq.com>
2016-03-24 16:03:06 +0800

    RxJava 6. 分解

--- app/src/main/java/com/user/hilo/RxJava/CatsHelper.java
+++ app/src/main/java/com/user/hilo/RxJava/CatsHelper.java
@@ -16,48 +16,7 @@ import java.util.List;
  */
 public class CatsHelper {
 
-//    4. 泛型回调
-    /*ApiWrapper apiWrapper;
-
-    // 这样写 错误的回调接口就会自动传递了. 当queryCats()方法发生错误时, 会通过onError回调,
-    // 当store()方法发生错误时,会通过调用者传递的Callback接口实现onError方式回调给调用者
-    public void saveTheCutestCat(String query, final Callback<Uri> cutestCatCallback) {
-        apiWrapper.queryCats(query, new Callback<List<Cat>>() {
-            @Override
-            public void onResult(List<Cat> result) {
-                Cat cat = findCutest(result);
-                apiWrapper.store(cat, cutestCatCallback);
-            }
-
-            @Override
-            public void onError(Exception e) {
-                Log.e("ERROR", "queryCats Error");
-                cutestCatCallback.onError(e);
-            }
-        });
-    }
-
-    private Cat findCutest(List<Cat> cats) {
-        return Collections.max(cats);
-    }*/
-
-    /**
-     * 可以看到比之前的简明了一些。
-     * 我们可以通过直接传递一个顶级的cutestCatCallback回调接口给apiWrapper.store来减少回调间的层级调用，
-     * 此外作为回调方法的签名是一样的。
-     * 但是我们可以做的更好！
-     * 5. 你必须把它分开
-     *  让我们来看看我们的异步操作（queryCats，queryCats，还有saveTheCutestCat），它们都遵循了相同的模式。
-     *  调用它们的方法有一些参数（query、cat）也包括一个回调对象。再次说明：任何异步操作需要携带所需的常规参数和一个回调实例对象。
-     *  如果我们试图去分开这几个阶段，每个异步操作仅仅将会携带一个参数对象，然后返回一些携带着回调（信息）的临时对象。
-        我们来应用下这样的模式，看看是否对我们有所帮助。
-        如果在异步操作中返回一些临时对象，我们需要定义一个出来。这样的一个对象需要包括常见的行为（以回调为单一参数），
-        我们将定义这样的类给所有的异步操作使用，这个类就叫它AsyncJob。
-        P.S. 称之为AsyncTask更合适一点，但是我不希望你混淆了异步操作跟另外一个存在的抽象概念之间的关系（这是不好的一点）。
-     */
-
-
-    ApiWrapper apiWrapper;
+  /*  ApiWrapper apiWrapper;
 
     public AsyncJob<Uri> saveTheCutestCat(final String query) {
         return new AsyncJob<Uri>() {
@@ -93,13 +52,77 @@ public class CatsHelper {
 
     private Cat findCutest(List<Cat> cats) {
         return Collections.max(cats);
-    }
+    }*/
     /**
-     * 哇，之前的版本更简单些啊，我们现在的优势是什么？
+     * 6. 哇，之前的版本更简单些啊，我们现在的优势是什么？
      * 答案就是现在我们可以给客户端返回“组合”操作的AsyncJob<Uri>。所以一个客户端（在 activity 或者 fragment 处）可以用组合起来的工作来操作。
      * 为了让我们的代码拥有之前的可读性，我们从这个事件流中强行进入到里面的操作里。但有件事需要注意，如果某些操作（方法）是异步的，
      * 然后调用它的操作（方法）又是异步的，就比如，查询猫的操作是异步的，然后寻找出最可爱的猫（即使有一个阻塞调用）也是一个异步操作（客户端希望接收的结果）。
      * 所以我们可以使用 AsyncJobs 把我们的方法分解成更小的操作：
      */
 
+    ApiWrapper apiWrapper;
+
+    public AsyncJob<Uri> saveTheCutestCat(String query) {
+        final AsyncJob<List<Cat>> asyncListcats = apiWrapper.queryCats(query);
+        final AsyncJob<Cat> asyncCat = new AsyncJob<Cat>() {
+            @Override
+            public void start(final Callback<Cat> callback) {
+                asyncListcats.start(new Callback<List<Cat>>() {
+                    @Override
+                    public void onResult(List<Cat> cats) {
+                        callback.onResult(findCutest(cats));
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        callback.onError(e);
+                    }
+                });
+            }
+        };
+
+        AsyncJob<Uri> asyncStoreUri = new AsyncJob<Uri>() {
+            @Override
+            public void start(final Callback<Uri> callback) {
+                asyncCat.start(new Callback<Cat>() {
+                    @Override
+                    public void onResult(Cat cat) {
+                        apiWrapper.store(cat)
+                                .start(new Callback<Uri>() {
+                                    @Override
+                                    public void onResult(Uri uri) {
+                                        callback.onResult(uri);
+                                    }
+
+                                    @Override
+                                    public void onError(Exception e) {
+                                        callback.onError(e);
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        callback.onError(e);
+                    }
+                });
+            }
+        };
+        return asyncStoreUri;
+    }
+
+    private Cat findCutest(List<Cat> cats) {
+        return Collections.max(cats);
+    }
+
+    /**
+     * 7. 代码量多了许多，但是更加清晰了。低层次嵌套的回调，利于理解的变量名（catsListAsyncJob、cutestCatAsyncJob、storedUriAsyncJob）。
+     * 看起来好了许多，但我们要再做一些事情：
+     * findCutest(result)只有一行是对我们有用（对于逻辑来说）的操作：
+     * 剩下的仅仅是开启另外一个AsyncJob和传递结果与错误的样板代码。此外，这些代码并不用于特定的任务，我们可以把其移动到其它地方而不影响编写我们真正需要的业务代码。
+     * 我们该怎么写呢？我们必须做下面的两件事情：
+     * AsyncJob是我们转换的结果转换方法这又有另外一个问题，
+     * 因为在 Java 中不能直接传递方法（函数）所以我们需要通过类（和接口）来间接实现这样的功能，然后我们就来定义这个 “方法”：
+     */
 }
--- app/src/main/java/com/user/hilo/RxJava/WebApi/ApiWrapper.java
+++ app/src/main/java/com/user/hilo/RxJava/WebApi/ApiWrapper.java
@@ -13,37 +13,6 @@ import java.util.List;
  */
 public class ApiWrapper {
 
-  /* 4. 泛型接口 Callback
-  Api api;
-
-    public void queryCats(String query, final Callback<List<Cat>> catCallback) {
-        api.queryCats(query, new Api.CatsQueryCallback() {
-            @Override
-            public void onCatsListReceived(List<Cat> cats) {
-                catCallback.onResult(cats);
-            }
-
-            @Override
-            public void onQueryFailed(Exception e) {
-                catCallback.onError(e);
-            }
-        });
-    }
-
-    public void store(Cat cat, final Callback<Uri> uriCallback) {
-        api.store(cat, new Api.StoreCallback() {
-            @Override
-            public void onCatStored(Uri uri) {
-                uriCallback.onResult(uri);
-            }
-
-            @Override
-            public void onStoreFailed(Exception e) {
-                uriCallback.onError(e);
-            }
-        });
-    }*/
-
     // 5. 返回回调（信息）的临时对象
     Api api;
 
